cac6ed7461e2d94dd94a25c2331c52d927798236
František Dvořák <valtri@civ.zcu.cz>
2015-01-11 02:17:33 +0100

    Missing directory required for hbase client (HBase 0.98.6).

--- manifests/frontend/config.pp
+++ manifests/frontend/config.pp
@@ -1,6 +1,19 @@
 class hbase::frontend::config {
   contain hbase::common::config
+
   if $hbase::external_zookeeper {
     contain hbase::common::keytab
+  }
+
+  file {'/var/lib/hbase/local':
+    ensure  => 'directory',
+    owner   => 'hbase',
+    group   => 'hbase',
+  }
+  ->
+  file {'/var/lib/hbase/local/jars':
+    ensure  => 'directory',
+    owner   => 'hbase',
+    group   => 'hbase',
   }
 }
