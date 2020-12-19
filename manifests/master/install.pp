7d629dd8c3610dec30209250e0d68e0c36281b1c
František Dvořák <valtri@civ.zcu.cz>
2020-12-19 23:48:24 +0100

    Workaround troubles with premature startup on Debian

--- manifests/master/install.pp
+++ manifests/master/install.pp
@@ -8,4 +8,26 @@ class hbase::master::install {
 
   ensure_packages($hbase::packages['master'])
   Package[$hbase::packages['master']] -> Class['hbase::common::postinstall']
+
+  # workaround troubles with premature startup on Debian
+  $daemon = $::hbase::daemons['master']
+  $path = '/sbin:/usr/sbin:/bin:/usr/bin'
+  case $::osfamily {
+    'debian': {
+      exec {"debian-workaround-${daemon}":
+        command => "echo '#! /bin/sh' > /etc/init.d/${daemon} && chmod +x /etc/init.d/${daemon}",
+        path    => $path,
+        creates => "/etc/init.d/${daemon}",
+      }
+      ->
+      Package[$hbase::packages['master']]
+      ->
+      exec{"debian-restore-${daemon}":
+        command => "mv -v /etc/init.d/${daemon}.dpkg-dist /etc/init.d/${daemon}",
+        path    => $path,
+        onlyif  => "test -f /etc/init.d/${daemon}.dpkg-dist",
+      }
+    }
+    default: {}
+  }
 }
--- manifests/regionserver/install.pp
+++ manifests/regionserver/install.pp
@@ -8,4 +8,26 @@ class hbase::regionserver::install {
 
   ensure_packages($hbase::packages['regionserver'])
   Package[$hbase::packages['regionserver']] -> Class['hbase::common::postinstall']
+
+  # workaround troubles with premature startup on Debian
+  $daemon = $::hbase::daemons['regionserver']
+  $path = '/sbin:/usr/sbin:/bin:/usr/bin'
+  case $::osfamily {
+    'debian': {
+      exec {"debian-workaround-${daemon}":
+        command => "echo '#! /bin/sh' > /etc/init.d/${daemon} && chmod +x /etc/init.d/${daemon}",
+        path    => $path,
+        creates => "/etc/init.d/${daemon}",
+      }
+      ->
+      Package[$hbase::packages['regionserver']]
+      ->
+      exec{"debian-restore-${daemon}":
+        command => "mv -v /etc/init.d/${daemon}.dpkg-dist /etc/init.d/${daemon}",
+        path    => $path,
+        onlyif  => "test -f /etc/init.d/${daemon}.dpkg-dist",
+      }
+    }
+    default: {}
+  }
 }
